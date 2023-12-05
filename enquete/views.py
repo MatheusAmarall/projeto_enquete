50ec337435bf418ffb13f966de011f87b23c5886
leonardothiesen <127902767+leonardothiesen@users.noreply.github.com>
2023-12-04 21:38:01 -0300

    ajustando models, views e tela

--- enquete/views.py
+++ enquete/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
+from .models import Enquetes
 
 def index(request):
     return render(request, 'pages/index.html')
@@ -10,6 +11,19 @@ def user_logout(request):
 
 def create(request):
     if request.method == 'POST':
-        pass
+        opcao_um = request.POST.get('opcao1')
+        opcao_dois = request.POST.get('opcao2')
+        opcao_tres = request.POST.get('opcao3')
+        opcao_quatro = request.POST.get('opcao4')
+
+
+        Enquetes.objects.create(
+            opcao_um=opcao_um,
+            opcao_dois=opcao_dois,
+            opcao_tres=opcao_tres,
+            opcao_quatro=opcao_quatro
+        )
+
+        return redirect('home')
     else:
         return render(request, 'pages/criacao_enquete.html')
